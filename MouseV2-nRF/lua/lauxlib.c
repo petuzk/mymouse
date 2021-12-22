48eee5bf9f91958f524af7cf6b2a9a30feaab9b0
Taras Radchenko <petuzk.dev@gmail.com>
2021-12-22 19:31:05 +0100

    [lua] Introduce Zephyr to Lua
    - Rewrite script loading from filesystem
      to operate on fs_file_t
    - Use hardware random number generator

--- MouseV2-nRF/lua/lauxlib.c
+++ MouseV2-nRF/lua/lauxlib.c
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <fs/fs.h>
 
 /*
 ** This file uses only the official API of Lua.
@@ -707,9 +708,16 @@ LUALIB_API void luaL_unref (lua_State *L, int t, int ref) {
 
 typedef struct LoadF {
   int n;  /* number of pre-read characters */
-  FILE *f;  /* file being read */
+  int error;
+  struct fs_file_t f;  /* file being read */
   char buff[BUFSIZ];  /* area for reading file */
 } LoadF;
+
+
+static int lfgetc(LoadF *lf) {
+  static unsigned char buf;
+  return fs_read(&lf->f, &buf, 1) > 0 ? buf : EOF;
+}
 
 
 static const char *getF (lua_State *L, void *ud, size_t *size) {
@@ -719,12 +727,17 @@ static const char *getF (lua_State *L, void *ud, size_t *size) {
     *size = lf->n;  /* return them (chars already in buffer) */
     lf->n = 0;  /* no more pre-read characters */
   }
-  else {  /* read a block from file */
-    /* 'fread' can return > 0 *and* set the EOF flag. If next call to
-       'getF' called 'fread', it might still wait for user input.
-       The next check avoids this problem. */
-    if (feof(lf->f)) return NULL;
-    *size = fread(lf->buff, 1, sizeof(lf->buff), lf->f);  /* read block */
+  else if (!lf->error) {  /* read a block from file */
+    ssize_t rv = fs_read(&lf->f, lf->buff, sizeof(lf->buff));  /* read block */
+    if (rv < 0) {
+      *size = 0;
+      lf->error = rv;
+    } else {
+      *size = rv;
+    }
+  }
+  else {
+    *size = 0;
   }
   return lf->buff;
 }
@@ -744,12 +757,12 @@ static int skipBOM (LoadF *lf) {
   int c;
   lf->n = 0;
   do {
-    c = getc(lf->f);
+    c = lfgetc(lf);
     if (c == EOF || c != *(const unsigned char *)p++) return c;
     lf->buff[lf->n++] = c;  /* to be read by the parser */
   } while (*p != '\0');
   lf->n = 0;  /* prefix matched; discard it */
-  return getc(lf->f);  /* return next character */
+  return lfgetc(lf);  /* return next character */
 }
 
 
@@ -764,9 +777,9 @@ static int skipcomment (LoadF *lf, int *cp) {
   int c = *cp = skipBOM(lf);
   if (c == '#') {  /* first line is a comment (Unix exec. file)? */
     do {  /* skip first line */
-      c = getc(lf->f);
+      c = lfgetc(lf);
     } while (c != EOF && c != '\n');
-    *cp = getc(lf->f);  /* skip end-of-line, if present */
+    *cp = lfgetc(lf);  /* skip end-of-line, if present */
     return 1;  /* there was a comment */
   }
   else return 0;  /* no comment */
@@ -776,31 +789,21 @@ static int skipcomment (LoadF *lf, int *cp) {
 LUALIB_API int luaL_loadfilex (lua_State *L, const char *filename,
                                              const char *mode) {
   LoadF lf;
-  int status, readstatus;
+  int status;
   int c;
   int fnameindex = lua_gettop(L) + 1;  /* index of filename on the stack */
-  if (filename == NULL) {
-    lua_pushliteral(L, "=stdin");
-    lf.f = stdin;
-  }
-  else {
-    lua_pushfstring(L, "@%s", filename);
-    lf.f = fopen(filename, "r");
-    if (lf.f == NULL) return errfile(L, "open", fnameindex);
-  }
+  lua_assert(filename != NULL);
+  lua_pushfstring(L, "@%s", filename);
+  fs_file_t_init(&lf.f);
+  lf.error = 0;
+  if (fs_open(&lf.f, filename, FS_O_READ)) return errfile(L, "open", fnameindex);
   if (skipcomment(&lf, &c))  /* read initial portion */
     lf.buff[lf.n++] = '\n';  /* add line to correct line numbers */
-  if (c == LUA_SIGNATURE[0] && filename) {  /* binary file? */
-    lf.f = freopen(filename, "rb", lf.f);  /* reopen in binary mode */
-    if (lf.f == NULL) return errfile(L, "reopen", fnameindex);
-    skipcomment(&lf, &c);  /* re-read initial portion */
-  }
   if (c != EOF)
     lf.buff[lf.n++] = c;  /* 'c' is the first character of the stream */
   status = lua_load(L, getF, &lf, lua_tostring(L, -1), mode);
-  readstatus = ferror(lf.f);
-  if (filename) fclose(lf.f);  /* close file (even in case of errors) */
-  if (readstatus) {
+  fs_close(&lf.f);  /* close file (even in case of errors) */
+  if (lf.error) {
     lua_settop(L, fnameindex);  /* ignore results from 'lua_load' */
     return errfile(L, "read", fnameindex);
   }
--- MouseV2-nRF/lua/luaconf.h
+++ MouseV2-nRF/lua/luaconf.h
@@ -11,6 +11,8 @@
 #include <limits.h>
 #include <stddef.h>
 
+#include "patches.h"
+
 
 /*
 ** ===================================================================
