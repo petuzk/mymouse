ac6ac4430dc10dd8d7e6a7685bf15ca9ea845b15
Taras Radchenko <petuzk.dev@gmail.com>
2023-01-16 21:34:38 +0100

    Implement GPIO edge detection ISR

--- app-nrf/include/platform/gpio.h
+++ app-nrf/include/platform/gpio.h
@@ -1,6 +1,36 @@
 #pragma once
 
+#include <stdbool.h>
+#include <stdint.h>
+
 #include <zephyr/devicetree.h>
+
+typedef void (*gpio_edge_cb)(uint32_t pin, bool new_level);
+
+/**
+ * @brief Enable one-time GPIO interrupt for the level change on given pins.
+ *
+ * Because the SENSE mechanism uses common DETECT signal for all pins, the
+ * pins are disconnected from this signal after the DETECT condition is met
+ * so that other pins could generate rising edge on DETECT signal as well.
+ * Therefore it is user's responsibility to re-configure one-time interrupt
+ * if needed.
+ *
+ * @param pinmask    Bitmask of pins to enable interrupt for
+ * @param from_level Pin levels to sense level change from (only bits which are set in @c pinmask are considered)
+ * @param callback   Callback to be called once the edge is detected (called from ISR context)
+ */
+void gpio_set_edge_cb_mask(uint32_t pinmask, uint32_t from_level, gpio_edge_cb callback);
+
+static inline void gpio_set_edge_cb(uint32_t pin, bool from_level, gpio_edge_cb callback) {
+    gpio_set_edge_cb_mask(1 << pin, from_level << pin, callback);
+}
+
+void gpio_unset_edge_cb_mask(uint32_t pinmask);
+
+static inline void gpio_unset_edge_cb(uint32_t pin) {
+    gpio_unset_edge_cb_mask(1 << pin);
+}
 
 #define PINOFPROP(nodelabel, propname) DT_GPIO_PIN(DT_NODELABEL(nodelabel), propname)
 #define PINOF(nodelabel) PINOFPROP(nodelabel, gpios)
--- app-nrf/src/platform/gpio.c
+++ app-nrf/src/platform/gpio.c
@@ -3,7 +3,13 @@
 #include <stdint.h>
 
 #include <hal/nrf_gpio.h>
+#include <hal/nrf_gpiote.h>
 #include <zephyr/init.h>
+#include <zephyr/irq.h>
+
+#include "util.h"
+
+#define GPIOTE_DT_NODE DT_NODELABEL(gpiote)
 
 const uint32_t button_inputs[] = {
     FOR_EACH(PINOF, (,),
@@ -24,6 +30,50 @@ const uint32_t outputs[] = {
         led_red),
 };
 
+static uint32_t gpio_int_enabled = 0; // bitmask of pins with enabled interrupts
+static uint32_t gpio_int_from_level;  // bitmask of pin levels before sensed edges
+static gpio_edge_cb callbacks[32];
+
+void gpio_set_edge_cb_mask(uint32_t pinmask, uint32_t from_level, gpio_edge_cb callback) {
+    unsigned key = irq_lock();
+
+    gpio_int_enabled |= pinmask;
+    gpio_int_from_level = (gpio_int_from_level & (~pinmask)) | (from_level & pinmask);
+    while (pinmask) {
+        uint32_t pin = get_next_bit_pos(&pinmask);
+        callbacks[pin] = callback;
+        nrf_gpio_cfg_sense_set(pin, (from_level & BIT(pin)) ? NRF_GPIO_PIN_SENSE_LOW : NRF_GPIO_PIN_SENSE_HIGH);
+    }
+
+    irq_unlock(key);
+}
+
+void gpio_unset_edge_cb_mask(uint32_t pinmask) {
+    unsigned key = irq_lock();
+
+    gpio_int_enabled &= ~pinmask;
+    while (pinmask) {
+        uint32_t pin = get_next_bit_pos(&pinmask);
+        nrf_gpio_cfg_sense_set(pin, NRF_GPIO_PIN_NOSENSE);
+    }
+
+    irq_unlock(key);
+}
+
+static void gpiote_irq_handler() {
+    // irq is called on PORT event only
+    nrf_gpiote_event_clear(NRF_GPIOTE, NRF_GPIOTE_EVENT_PORT);
+
+    uint32_t levels = nrf_gpio_port_in_read(NRF_P0);
+    uint32_t interrupts_from = (levels ^ gpio_int_from_level) & gpio_int_enabled;
+    gpio_int_enabled &= ~interrupts_from;
+    while (interrupts_from) {
+        uint32_t pin = get_next_bit_pos(&interrupts_from);
+        nrf_gpio_cfg_sense_set(pin, NRF_GPIO_PIN_NOSENSE);
+        callbacks[pin](pin, (levels >> pin) & 1);
+    }
+}
+
 static int gpio_init(const struct device *dev) {
     ARG_UNUSED(dev);
 
@@ -35,6 +85,11 @@ static int gpio_init(const struct device *dev) {
         nrf_gpio_pin_clear(outputs[i]);
         nrf_gpio_cfg_output(outputs[i]);
     }
+
+    nrf_gpiote_event_clear(NRF_GPIOTE, NRF_GPIOTE_EVENT_PORT);
+    nrf_gpiote_int_enable(NRF_GPIOTE, NRF_GPIOTE_INT_PORT_MASK);
+    IRQ_CONNECT(DT_IRQN(GPIOTE_DT_NODE), DT_IRQ(GPIOTE_DT_NODE, priority), gpiote_irq_handler, NULL, 0);
+    irq_enable(DT_IRQN(GPIOTE_DT_NODE));
 
     return 0;
 }
