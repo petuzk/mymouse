182bdf2207b0268a8a2b961513377ff61d017834
Taras Radchenko <petuzk.dev@gmail.com>
2024-07-24 21:04:33 +0200

    Convert tabs to spaces

--- app-avr/src/Descriptors.h
+++ app-avr/src/Descriptors.h
@@ -36,57 +36,57 @@
 #ifndef _DESCRIPTORS_H_
 #define _DESCRIPTORS_H_
 
-	/* Includes: */
-		#include <LUFA/Drivers/USB/USB.h>
+    /* Includes: */
+        #include <LUFA/Drivers/USB/USB.h>
 
-		#include <avr/pgmspace.h>
+        #include <avr/pgmspace.h>
 
-	/* Macros: */
-		/** Endpoint address of the Mouse HID reporting IN endpoint. */
-		#define MOUSE_EPADDR              (ENDPOINT_DIR_IN | 1)
+    /* Macros: */
+        /** Endpoint address of the Mouse HID reporting IN endpoint. */
+        #define MOUSE_EPADDR              (ENDPOINT_DIR_IN | 1)
 
-		/** Size in bytes of the Mouse HID reporting IN endpoint. */
-		#define MOUSE_EPSIZE              8
+        /** Size in bytes of the Mouse HID reporting IN endpoint. */
+        #define MOUSE_EPSIZE              8
 
-	/* Type Defines: */
-		/** Type define for the device configuration descriptor structure. This must be defined in the
-		 *  application code, as the configuration descriptor contains several sub-descriptors which
-		 *  vary between devices, and which describe the device's usage to the host.
-		 */
-		typedef struct
-		{
-			USB_Descriptor_Configuration_Header_t Config;
+    /* Type Defines: */
+        /** Type define for the device configuration descriptor structure. This must be defined in the
+         *  application code, as the configuration descriptor contains several sub-descriptors which
+         *  vary between devices, and which describe the device's usage to the host.
+         */
+        typedef struct
+        {
+            USB_Descriptor_Configuration_Header_t Config;
 
-			// Mouse HID Interface
-			USB_Descriptor_Interface_t            HID_Interface;
-			USB_HID_Descriptor_HID_t              HID_MouseHID;
-			USB_Descriptor_Endpoint_t             HID_ReportINEndpoint;
-		} USB_Descriptor_Configuration_t;
+            // Mouse HID Interface
+            USB_Descriptor_Interface_t            HID_Interface;
+            USB_HID_Descriptor_HID_t              HID_MouseHID;
+            USB_Descriptor_Endpoint_t             HID_ReportINEndpoint;
+        } USB_Descriptor_Configuration_t;
 
-		/** Enum for the device interface descriptor IDs within the device. Each interface descriptor
-		 *  should have a unique ID index associated with it, which can be used to refer to the
-		 *  interface from other descriptors.
-		 */
-		enum InterfaceDescriptors_t
-		{
-			INTERFACE_ID_Mouse = 0, /**< Mouse interface descriptor ID */
-		};
+        /** Enum for the device interface descriptor IDs within the device. Each interface descriptor
+         *  should have a unique ID index associated with it, which can be used to refer to the
+         *  interface from other descriptors.
+         */
+        enum InterfaceDescriptors_t
+        {
+            INTERFACE_ID_Mouse = 0, /**< Mouse interface descriptor ID */
+        };
 
-		/** Enum for the device string descriptor IDs within the device. Each string descriptor should
-		 *  have a unique ID index associated with it, which can be used to refer to the string from
-		 *  other descriptors.
-		 */
-		enum StringDescriptors_t
-		{
-			STRING_ID_Language     = 0, /**< Supported Languages string descriptor ID (must be zero) */
-			STRING_ID_Manufacturer = 1, /**< Manufacturer string ID */
-			STRING_ID_Product      = 2, /**< Product string ID */
-		};
+        /** Enum for the device string descriptor IDs within the device. Each string descriptor should
+         *  have a unique ID index associated with it, which can be used to refer to the string from
+         *  other descriptors.
+         */
+        enum StringDescriptors_t
+        {
+            STRING_ID_Language     = 0, /**< Supported Languages string descriptor ID (must be zero) */
+            STRING_ID_Manufacturer = 1, /**< Manufacturer string ID */
+            STRING_ID_Product      = 2, /**< Product string ID */
+        };
 
-	/* Function Prototypes: */
-		uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
-		                                    const uint16_t wIndex,
-		                                    const void** const DescriptorAddress)
-		                                    ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
+    /* Function Prototypes: */
+        uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
+                                            const uint16_t wIndex,
+                                            const void** const DescriptorAddress)
+                                            ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
 
 #endif
--- app-avr/src/Mouse.c
+++ app-avr/src/Mouse.c
@@ -58,22 +58,22 @@ static uint16_t IdleMSRemaining = 0;
  */
 int main(void)
 {
-	SetupHardware();
+    SetupHardware();
 
-	GlobalInterruptEnable();
+    GlobalInterruptEnable();
 
-	for (;;)
-	{
-		Mouse_Task();
-		USB_USBTask();
-	}
+    for (;;)
+    {
+        Mouse_Task();
+        USB_USBTask();
+    }
 }
 
 /** Configures the board hardware and chip peripherals for the demo's functionality. */
 void SetupHardware(void)
 {
-	/* Hardware Initialization */
-	USB_Init();
+    /* Hardware Initialization */
+    USB_Init();
 }
 
 /** Event handler for the USB_Connect event. This indicates that the device is enumerating via the status LEDs and
@@ -81,8 +81,8 @@ void SetupHardware(void)
  */
 void EVENT_USB_Device_Connect(void)
 {
-	/* Default to report protocol on connect */
-	UsingReportProtocol = true;
+    /* Default to report protocol on connect */
+    UsingReportProtocol = true;
 }
 
 /** Event handler for the USB_Disconnect event. This indicates that the device is no longer connected to a host via
@@ -95,13 +95,13 @@ void EVENT_USB_Device_Disconnect(void) {}
  */
 void EVENT_USB_Device_ConfigurationChanged(void)
 {
-	bool ConfigSuccess = true;
+    bool ConfigSuccess = true;
 
-	/* Setup HID Report Endpoint */
-	ConfigSuccess &= Endpoint_ConfigureEndpoint(MOUSE_EPADDR, EP_TYPE_INTERRUPT, MOUSE_EPSIZE, 1);
+    /* Setup HID Report Endpoint */
+    ConfigSuccess &= Endpoint_ConfigureEndpoint(MOUSE_EPADDR, EP_TYPE_INTERRUPT, MOUSE_EPSIZE, 1);
 
-	/* Turn on Start-of-Frame events for tracking HID report period expiry */
-	USB_Device_EnableSOFEvents();
+    /* Turn on Start-of-Frame events for tracking HID report period expiry */
+    USB_Device_EnableSOFEvents();
 }
 
 /** Event handler for the USB_ControlRequest event. This is used to catch and process control requests sent to
@@ -110,85 +110,85 @@ void EVENT_USB_Device_ConfigurationChanged(void)
  */
 void EVENT_USB_Device_ControlRequest(void)
 {
-	/* Handle HID Class specific requests */
-	switch (USB_ControlRequest.bRequest)
-	{
-		case HID_REQ_GetReport:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				USB_MouseReport_Data_t MouseReportData;
+    /* Handle HID Class specific requests */
+    switch (USB_ControlRequest.bRequest)
+    {
+        case HID_REQ_GetReport:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                USB_MouseReport_Data_t MouseReportData;
 
-				/* Create the next mouse report for transmission to the host */
-				CreateMouseReport(&MouseReportData);
+                /* Create the next mouse report for transmission to the host */
+                CreateMouseReport(&MouseReportData);
 
-				Endpoint_ClearSETUP();
+                Endpoint_ClearSETUP();
 
-				/* Write the report data to the control endpoint */
-				Endpoint_Write_Control_Stream_LE(&MouseReportData, sizeof(MouseReportData));
-				Endpoint_ClearOUT();
+                /* Write the report data to the control endpoint */
+                Endpoint_Write_Control_Stream_LE(&MouseReportData, sizeof(MouseReportData));
+                Endpoint_ClearOUT();
 
-				/* Clear the report data afterwards */
-				memset(&MouseReportData, 0, sizeof(MouseReportData));
-			}
+                /* Clear the report data afterwards */
+                memset(&MouseReportData, 0, sizeof(MouseReportData));
+            }
 
-			break;
-		case HID_REQ_GetProtocol:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
+            break;
+        case HID_REQ_GetProtocol:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
 
-				/* Write the current protocol flag to the host */
-				Endpoint_Write_8(UsingReportProtocol);
+                /* Write the current protocol flag to the host */
+                Endpoint_Write_8(UsingReportProtocol);
 
-				Endpoint_ClearIN();
-				Endpoint_ClearStatusStage();
-			}
+                Endpoint_ClearIN();
+                Endpoint_ClearStatusStage();
+            }
 
-			break;
-		case HID_REQ_SetProtocol:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
-				Endpoint_ClearStatusStage();
+            break;
+        case HID_REQ_SetProtocol:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
+                Endpoint_ClearStatusStage();
 
-				/* Set or clear the flag depending on what the host indicates that the current Protocol should be */
-				UsingReportProtocol = (USB_ControlRequest.wValue != 0);
-			}
+                /* Set or clear the flag depending on what the host indicates that the current Protocol should be */
+                UsingReportProtocol = (USB_ControlRequest.wValue != 0);
+            }
 
-			break;
-		case HID_REQ_SetIdle:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
-				Endpoint_ClearStatusStage();
+            break;
+        case HID_REQ_SetIdle:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_HOSTTODEVICE | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
+                Endpoint_ClearStatusStage();
 
-				/* Get idle period in MSB, must multiply by 4 to get the duration in milliseconds */
-				IdleCount = ((USB_ControlRequest.wValue & 0xFF00) >> 6);
-			}
+                /* Get idle period in MSB, must multiply by 4 to get the duration in milliseconds */
+                IdleCount = ((USB_ControlRequest.wValue & 0xFF00) >> 6);
+            }
 
-			break;
-		case HID_REQ_GetIdle:
-			if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
-			{
-				Endpoint_ClearSETUP();
+            break;
+        case HID_REQ_GetIdle:
+            if (USB_ControlRequest.bmRequestType == (REQDIR_DEVICETOHOST | REQTYPE_CLASS | REQREC_INTERFACE))
+            {
+                Endpoint_ClearSETUP();
 
-				/* Write the current idle duration to the host, must be divided by 4 before sent to host */
-				Endpoint_Write_8(IdleCount >> 2);
+                /* Write the current idle duration to the host, must be divided by 4 before sent to host */
+                Endpoint_Write_8(IdleCount >> 2);
 
-				Endpoint_ClearIN();
-				Endpoint_ClearStatusStage();
-			}
+                Endpoint_ClearIN();
+                Endpoint_ClearStatusStage();
+            }
 
-			break;
-	}
+            break;
+    }
 }
 
 /** Event handler for the USB device Start Of Frame event. */
 void EVENT_USB_Device_StartOfFrame(void)
 {
-	/* One millisecond has elapsed, decrement the idle time remaining counter if it has not already elapsed */
-	if (IdleMSRemaining)
-	  IdleMSRemaining--;
+    /* One millisecond has elapsed, decrement the idle time remaining counter if it has not already elapsed */
+    if (IdleMSRemaining)
+      IdleMSRemaining--;
 }
 
 /** Fills the given HID report data structure with the next HID report to send to the host.
@@ -197,63 +197,63 @@ void EVENT_USB_Device_StartOfFrame(void)
  */
 void CreateMouseReport(USB_MouseReport_Data_t* const ReportData)
 {
-	/* Clear the report contents */
-	memset(ReportData, 0, sizeof(USB_MouseReport_Data_t));
-	ReportData->X = 1;
+    /* Clear the report contents */
+    memset(ReportData, 0, sizeof(USB_MouseReport_Data_t));
+    ReportData->X = 1;
 }
 
 /** Sends the next HID report to the host, via the keyboard data endpoint. */
 void SendNextReport(void)
 {
-	static USB_MouseReport_Data_t PrevMouseReportData;
-	USB_MouseReport_Data_t        MouseReportData;
-	bool                          SendReport;
+    static USB_MouseReport_Data_t PrevMouseReportData;
+    USB_MouseReport_Data_t        MouseReportData;
+    bool                          SendReport;
 
-	/* Create the next mouse report for transmission to the host */
-	CreateMouseReport(&MouseReportData);
+    /* Create the next mouse report for transmission to the host */
+    CreateMouseReport(&MouseReportData);
 
-	/* Check to see if the report data has changed - if so a report MUST be sent */
-	SendReport = (memcmp(&PrevMouseReportData, &MouseReportData, sizeof(USB_MouseReport_Data_t)) != 0);
+    /* Check to see if the report data has changed - if so a report MUST be sent */
+    SendReport = (memcmp(&PrevMouseReportData, &MouseReportData, sizeof(USB_MouseReport_Data_t)) != 0);
 
-	/* Override the check if the Y or X values are non-zero - we want continuous movement while the joystick
-	 * is being held down (via continuous reports), otherwise the cursor will only move once per joystick toggle */
-	if ((MouseReportData.Y != 0) || (MouseReportData.X != 0))
-	  SendReport = true;
+    /* Override the check if the Y or X values are non-zero - we want continuous movement while the joystick
+     * is being held down (via continuous reports), otherwise the cursor will only move once per joystick toggle */
+    if ((MouseReportData.Y != 0) || (MouseReportData.X != 0))
+      SendReport = true;
 
-	/* Check if the idle period is set and has elapsed */
-	if (IdleCount && (!(IdleMSRemaining)))
-	{
-		/* Reset the idle time remaining counter */
-		IdleMSRemaining = IdleCount;
+    /* Check if the idle period is set and has elapsed */
+    if (IdleCount && (!(IdleMSRemaining)))
+    {
+        /* Reset the idle time remaining counter */
+        IdleMSRemaining = IdleCount;
 
-		/* Idle period is set and has elapsed, must send a report to the host */
-		SendReport = true;
-	}
+        /* Idle period is set and has elapsed, must send a report to the host */
+        SendReport = true;
+    }
 
-	/* Select the Mouse Report Endpoint */
-	Endpoint_SelectEndpoint(MOUSE_EPADDR);
+    /* Select the Mouse Report Endpoint */
+    Endpoint_SelectEndpoint(MOUSE_EPADDR);
 
-	/* Check if Mouse Endpoint Ready for Read/Write and if we should send a new report */
-	if (Endpoint_IsReadWriteAllowed() && SendReport)
-	{
-		/* Save the current report data for later comparison to check for changes */
-		PrevMouseReportData = MouseReportData;
+    /* Check if Mouse Endpoint Ready for Read/Write and if we should send a new report */
+    if (Endpoint_IsReadWriteAllowed() && SendReport)
+    {
+        /* Save the current report data for later comparison to check for changes */
+        PrevMouseReportData = MouseReportData;
 
-		/* Write Mouse Report Data */
-		Endpoint_Write_Stream_LE(&MouseReportData, sizeof(MouseReportData), NULL);
+        /* Write Mouse Report Data */
+        Endpoint_Write_Stream_LE(&MouseReportData, sizeof(MouseReportData), NULL);
 
-		/* Finalize the stream transfer to send the last packet */
-		Endpoint_ClearIN();
-	}
+        /* Finalize the stream transfer to send the last packet */
+        Endpoint_ClearIN();
+    }
 }
 
 /** Task to manage HID report generation and transmission to the host, when in report mode. */
 void Mouse_Task(void)
 {
-	/* Device must be connected and configured for the task to run */
-	if (USB_DeviceState != DEVICE_STATE_Configured)
-	  return;
+    /* Device must be connected and configured for the task to run */
+    if (USB_DeviceState != DEVICE_STATE_Configured)
+      return;
 
-	/* Send the next mouse report to the host */
-	SendNextReport();
+    /* Send the next mouse report to the host */
+    SendNextReport();
 }
