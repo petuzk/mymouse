49d9935a0b9c386692f0051cc816f2c305147c9c
Taras Radchenko <petuzk.dev@gmail.com>
2024-07-28 21:46:27 +0200

    Rewrite main, add flash command

--- app-avr/hidcli/src/hidcli/__main__.py
+++ app-avr/hidcli/src/hidcli/__main__.py
@@ -1,9 +1,22 @@
-import sys
+from argparse import ArgumentParser, FileType
 
 from hidcli import find_device
+from hidcli.device import DeviceMode
+from hidcli.intelhex_reader import load_program
 
 
 def main():
+    parser = ArgumentParser()
+    subparsers = parser.add_subparsers(help='Subcommands')
+
+    flash_parser = subparsers.add_parser('flash', help='Flash an application image via HID bootloader')
+    flash_parser.add_argument('image', type=FileType(), help='Path to .hex file (or "-" to read from stdin)')
+
+    mode_parser = subparsers.add_parser('mode', help='Change device mode')
+    mode_parser.add_argument('mode', choices=DeviceMode, help='Desired mode')
+
+    args = parser.parse_args()
+
     dev = find_device()
     if dev is None:
         print('Could not find the device.')
@@ -12,21 +25,22 @@ def main():
     with dev:
         print(f'Device is in {dev.mode} mode.')
 
-        if '-b' in sys.argv:
-            if dev.mode == 'app':
-                print('Entering bootloader')
-                dev.mode = 'bootloader'
-                print(f'Device is in {dev.mode} mode.')
+        if hasattr(args, 'mode'):
+            if dev.mode == args.mode:
+                print(f'Skipping changing device mode to {args.mode}')
             else:
-                print('Device is already in bootloader, ignoring -b')
+                print('Entering bootloader' if args.mode == DeviceMode.BOOTLOADER else 'Leaving bootloader')
+                dev.mode = args.mode
+                print(f'Device is in {dev.mode} mode.')
+            return
 
-        if '-a' in sys.argv:
-            if dev.mode == 'bootloader':
-                print('Leaving bootloader')
-                dev.mode = 'app'
-                print(f'Device is in {dev.mode} mode.')
-            else:
-                print('Device is already in app, ignoring -a')
+        if hasattr(args, 'image'):
+            print(f'Flashing device from {args.image.name}')
+            dev.bootloader().flash(*load_program(args.image))
+            print(f'Flashing completed, switching to app')
+            dev.mode = DeviceMode.APP
+            print(f'Device is in {dev.mode} mode.')
+            return
 
 
 main()
