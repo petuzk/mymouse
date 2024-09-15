fe998530f663c186165a94fe4144b8f769fedd33
Taras Radchenko <petuzk.dev@gmail.com>
2024-09-15 18:36:00 +0200

    Add missing #pragma once

--- app-avr/src/spi.h
+++ app-avr/src/spi.h
@@ -1,2 +1,4 @@
+#pragma once
+
 void init_spi();
 void spi_task();
--- app-avr/src/spi_commands.h
+++ app-avr/src/spi_commands.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdint.h>
 
 uint8_t prepare_response(uint8_t command_id, uint8_t* num_tx, const uint8_t** tx_data, uint8_t* num_rx, uint8_t** rx_data);
