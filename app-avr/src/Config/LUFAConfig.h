4b72eadab5e7cfe84c5088e30bd8173d4018f334
Taras Radchenko <petuzk.dev@gmail.com>
2024-09-22 11:39:29 +0200

    Reduce USB_STREAM_TIMEOUT_MS from 100 to 20
    100 is a default value. However, since the endpoint descriptor
    requests polling interval to be 5 ms, waiting for IN token for
    at most 20 ms should be sufficient. This waiting is blocking, so
    during wait time AVR does not respond to SPI -- hence we want this
    as low as possible.

--- app-avr/src/Config/LUFAConfig.h
+++ app-avr/src/Config/LUFAConfig.h
@@ -61,7 +61,7 @@
         #define USE_STATIC_OPTIONS               (USB_DEVICE_OPT_FULLSPEED | USB_OPT_REG_ENABLED | USB_OPT_AUTO_PLL)
         #define USB_DEVICE_ONLY
         // #define USB_HOST_ONLY
-        // #define USB_STREAM_TIMEOUT_MS            {Insert Value Here}
+        #define USB_STREAM_TIMEOUT_MS            20
         // #define NO_LIMITED_CONTROLLER_CONNECT
         // #define NO_SOF_EVENTS
 
